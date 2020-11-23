b4714c163d5317cd32f01e5f6d364857b93b69dd
米生成 <1295798019@qq.com>
2020-11-23 19:16:28 +0800

    ceshi

---
+++ src/main/java/com/msc/springcloud_eurekademo/test/test.java
@@ -0,0 +1,14 @@
+﻿package com.msc.springcloud_eurekademo.test;
+
+/**
+ * @author Misc
+ * @create 2020-11-23 19:15
+ * @PACKAGE_NAME com.msc.springcloud_eurekademo.test
+ * @PROJECT_NAME springcloud_eurekademo
+ */
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("nihao");
+    }
+}
